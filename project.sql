0d09e42685c1d539eb89cff8708a542cdbb4df9b
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-09-09 17:31:51 +0530

    updated

---
+++ project.sql
@@ -0,0 +1,47 @@
+
+-- Project Understanding 
+-- GLUE : ETL 
+Q) What was the business problem you were solving, and why did you choose these specific AWS services?
+
+
+-- 1. Source
+
+Q) End to end flow 
+Q) Data Size 
+Q) What is data source 
+A) Batch processing vs Real time streaming 
+
+-- 2. Data Storage 
+   * Raw 
+   * Refind 
+   * 
+
+-- 3. Data Transformation 
+
+
+-- 4. Destination 
+Q) What is data destination 
+
+
+--- 5. Consumption 
+
+Database => Redshift
+======================
+1. Cluster Size 
+2. Running state => 24/7 
+3. How do you connect to cluster ?
+4. No of tables 
+  * Dimensions Table 
+  * Fact table 
+6. Schema Type
+6. Schema Evoution 
+
+
+--- 3. INFRA SET 
+Q) DEV/UAT/PROD  ENV
+Q) tools used 
+
+-- 4. Deployment 
+
+
+-- 5. Testing 
